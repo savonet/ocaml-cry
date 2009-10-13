c65abe42ce67c8e4549c89598fdfe3b0da83fd30
metamorph68@aec24677-d710-0410-a355-ac75e2bdf181 <metamorph68@aec24677-d710-0410-a355-ac75e2bdf181>
2009-10-13 01:21:07 +0000

    Prepared ocaml-cry for release (0.1.0)
    --HG--
    extra : convert_revision : svn%3Aaec24677-d710-0410-a355-ac75e2bdf181/trunk%406813

--- examples/connect.ml
+++ examples/connect.ml
@@ -10,10 +10,9 @@ let connection =
   let samplerate = 44100 in
   let bitrate = 128 in
   let protocol = Icy in
-  let icy = true in
   let audio_info = audio_info ~channels ~samplerate ~bitrate () in 
   {
-    (connection ~audio_info ~icy ~protocol ~mount ~content_type ()) with
+    (connection ~audio_info ~protocol ~mount ~content_type ()) with
     port = 8005
   }
 
@@ -22,8 +21,8 @@ let () =
     connect c connection;
     if get_icy_cap c then Printf.printf "Icy enabled !\n%!";
     let meta = Hashtbl.create 10 in
-    Hashtbl.add meta "artist" "testé";
-    Hashtbl.add meta "title"  "testé";
+    Hashtbl.add meta "artist" "test";
+    Hashtbl.add meta "title"  "test";
     update_metadata c meta
   with
     | Error e -> Printf.printf "Error: %s\n%!" (string_of_error e)
