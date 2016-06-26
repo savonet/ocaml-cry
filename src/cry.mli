4433bafe52e1b4b46ae23fd5e6a4ad5722440295
Romain Beauxis <toots@rastageeks.org>
2016-06-26 10:28:11 -0500

    Forgot this one

--- src/cry.mli
+++ src/cry.mli
@@ -36,8 +36,8 @@
 type operation = [`Read|`Write|`Both]
 
 type transport = {
-  write: bytes -> int -> int -> int;
-  read: bytes -> int -> int -> int;
+  write: Bytes.t -> int -> int -> int;
+  read: Bytes.t -> int -> int -> int;
   wait_for : operation -> float -> bool;
   close: unit -> unit
 }
